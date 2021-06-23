3dbc253997f393a7dd217931a8a55a2b10dd968d
TOny Baglien <tony.b.composition@gmail.com>
2021-06-23 14:58:37 -0500

    added js and scss

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+//Select Dom Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menu.classList.add('show');
+    menuBtn.classList.add('close');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach((item) => {
+      item.classList.add('show');
+    });
+    //Set menu state
+    showMenu = true;
+  } else {
+    menu.classList.remove('show');
+    menuBtn.classList.remove('close');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach((item) => {
+      item.classList.remove('show');
+    });
+    //Set menu state
+    showMenu = false;
+  }
+}
